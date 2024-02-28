2031fff3e44b63b35cda368516805570ae4c0fc0
CarlosRobinDeCloet <carlosdecloet@gmail.com>
2024-02-28 20:33:56 +0100

    working navburger submenu for only 1 submenu

--- script.js
+++ script.js
@@ -1,9 +1,15 @@
 const hamburger = document.querySelector(".hamburger");
-const navMenu = document.querySelector(".nav-menu");
+const navMenu = document.querySelector(".nav-menu-mobile");
+const subNavMenu = document.querySelector(".nav-submenu-mobile");
+const subNavMenuList = document.querySelector(".nav-submenu-list-mobile");
 
 hamburger.addEventListener("click", () => {
     hamburger.classList.toggle("active");
     navMenu.classList.toggle("active");
+})
+
+subNavMenu.addEventListener("click", () => {
+    subNavMenuList.classList.toggle("active");
 })
 
 document.querySelectorAll(".nav-link").forEach(n => 
