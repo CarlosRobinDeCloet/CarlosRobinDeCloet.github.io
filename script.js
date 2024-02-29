d6b0664077c8266077f86f6e699bb9f0faa48645
CarlosRobinDeCloet <carlosdecloet@gmail.com>
2024-02-29 19:44:23 +0100

    added all functionalities to mobile navigation bar

--- script.js
+++ script.js
@@ -1,15 +1,33 @@
 const hamburger = document.querySelector(".hamburger");
 const navMenu = document.querySelector(".nav-menu-mobile");
-const subNavMenu = document.querySelector(".nav-submenu-mobile");
-const subNavMenuList = document.querySelector(".nav-submenu-list-mobile");
+const subNavMenu1 = document.getElementById("nav-submenu-mobile1");
+const subNavMenu2 = document.getElementById("nav-submenu-mobile2");
+const subNavMenu3 = document.getElementById("nav-submenu-mobile3");
+const subNavMenu4 = document.getElementById("nav-submenu-mobile4");
+const subNavMenuList1 = document.getElementById("nav-submenu-list-mobile1");
+const subNavMenuList2 = document.getElementById("nav-submenu-list-mobile2");
+const subNavMenuList3 = document.getElementById("nav-submenu-list-mobile3");
+const subNavMenuList4 = document.getElementById("nav-submenu-list-mobile4");
 
 hamburger.addEventListener("click", () => {
     hamburger.classList.toggle("active");
     navMenu.classList.toggle("active");
 })
 
-subNavMenu.addEventListener("click", () => {
-    subNavMenuList.classList.toggle("active");
+subNavMenu1.addEventListener("click", () => {
+    subNavMenuList1.classList.toggle("active");
+})
+
+subNavMenu2.addEventListener("click", () => {
+    subNavMenuList2.classList.toggle("active");
+})
+
+subNavMenu3.addEventListener("click", () => {
+    subNavMenuList3.classList.toggle("active");
+})
+
+subNavMenu4.addEventListener("click", () => {
+    subNavMenuList4.classList.toggle("active");
 })
 
 document.querySelectorAll(".nav-link").forEach(n => 
