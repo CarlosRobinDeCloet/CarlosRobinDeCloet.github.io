733d46c4c57c8ce8042c50599c3d0abe53f582cd
CarlosRobinDeCloet <carlosdecloet@gmail.com>
2024-05-14 11:09:59 +0200

    changes to js

--- script.js
+++ script.js
@@ -4,10 +4,12 @@ const subNavMenu1 = document.getElementById("nav-submenu-mobile1");
 const subNavMenu2 = document.getElementById("nav-submenu-mobile2");
 const subNavMenu3 = document.getElementById("nav-submenu-mobile3");
 const subNavMenu4 = document.getElementById("nav-submenu-mobile4");
+const subNavMenu5 = document.getElementById("nav-submenu-mobile5");
 const subNavMenuList1 = document.getElementById("nav-submenu-list-mobile1");
 const subNavMenuList2 = document.getElementById("nav-submenu-list-mobile2");
 const subNavMenuList3 = document.getElementById("nav-submenu-list-mobile3");
 const subNavMenuList4 = document.getElementById("nav-submenu-list-mobile4");
+const subNavMenuList5 = document.getElementById("nav-submenu-list-mobile5");
 
 hamburger.addEventListener("click", () => {
     hamburger.classList.toggle("active");
@@ -28,6 +30,10 @@ subNavMenu3.addEventListener("click", () => {
 
 subNavMenu4.addEventListener("click", () => {
     subNavMenuList4.classList.toggle("active");
+})
+
+subNavMenu4.addEventListener("click", () => {
+    subNavMenuList5.classList.toggle("active");
 })
 
 document.querySelectorAll(".nav-link").forEach(n => 
