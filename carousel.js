e9084262e4c3e7ee508de2ed1384c30084fc3d82
CarlosRobinDeCloet <carlosdecloet@gmail.com>
2024-06-03 15:35:23 +0200

    check if fixes things

--- carousel.js
+++ carousel.js
@@ -1,71 +1,71 @@
 
 const carousel1 = document.getElementById("carousel1");
-    const items = carousel1.children;
-    const buttonsHtml = Array.from(items, () => {
+    const items1 = carousel1.children;
+    const buttonsHtml1 = Array.from(items1, () => {
         return `<span class='carousel__button'></span>`;
     });
 
-    console.log(items);
-    console.log(buttonsHtml);
+    console.log(items1);
+    console.log(buttonsHtml1);
 
     carousel1.insertAdjacentHTML('beforeend', `
         <div class='carousel__nav'>
-            ${ buttonsHtml.join('')}
+            ${ buttonsHtml1.join('')}
         </div>
     `);
 
-    const buttons = carousel1.querySelectorAll('.carousel__button');
+    const buttons1 = carousel1.querySelectorAll('.carousel__button');
 
-    console.log(buttons);
+    console.log(buttons1);
 
-    buttons.forEach((button, i) => {
+    buttons1.forEach((button, i) => {
         console.log(i);
         button.addEventListener('click', () => {
-            items.forEach(item => item.classList.remove('carousel__item__selected'));
-            buttons.forEach(button => button.classList.remove('carousel__button__selected'));
+            items1.forEach(item => item.classList.remove('carousel__item__selected'));
+            buttons1.forEach(button => button.classList.remove('carousel__button__selected'));
 
-            items[i].classList.add('carousel__item__selected');
-            buttons[i].classList.add('carousel__button__selected');
+            items1[i].classList.add('carousel__item__selected');
+            buttons1[i].classList.add('carousel__button__selected');
         });
     });
 
 // select first item on page load
-items[0].classList.add('carousel__item__selected');
-buttons[0].classList.add('carousel__button__selected');
+items1[0].classList.add('carousel__item__selected');
+buttons1[0].classList.add('carousel__button__selected');
 
 
-document.getElementById("carousel2").forEach(carousel=> {
-    const items = carousel.querySelectorAll(".carousel__item");
-    const buttonsHtml = Array.from(items, () => {
+const carousel2 = document.getElementById("carousel2");
+    const items2 = carousel2.children;
+    const buttonsHtml2 = Array.from(items2, () => {
         return `<span class='carousel__button'></span>`;
     });
 
-    console.log(items);
-    console.log(buttonsHtml);
+    console.log(items2);
+    console.log(buttonsHtml2);
 
     carousel2.insertAdjacentHTML('beforeend', `
         <div class='carousel__nav'>
-            ${ buttonsHtml.join('')}
+            ${ buttonsHtml2.join('')}
         </div>
     `);
 
-    const buttons = carousel1.querySelectorAll('.carousel__button');
+    const buttons2 = carousel2.querySelectorAll('.carousel__button');
 
-    console.log(buttons);
+    console.log(buttons2);
 
-    buttons.forEach((button, i) => {
+    buttons2.forEach((button, i) => {
         console.log(i);
         button.addEventListener('click', () => {
-            items.forEach(item => item.classList.remove('carousel__item__selected'));
-            buttons.forEach(button => button.classList.remove('carousel__button__selected'));
+            items2.forEach(item => item.classList.remove('carousel__item__selected'));
+            buttons2.forEach(button => button.classList.remove('carousel__button__selected'));
 
-            items[i].classList.add('carousel__item__selected');
-            buttons[i].classList.add('carousel__button__selected');
+            items2[i].classList.add('carousel__item__selected');
+            buttons2[i].classList.add('carousel__button__selected');
         });
     });
 
     // select first item on page load
-    items[0].classList.add('carousel__item__selected');
-    buttons[0].classList.add('carousel__button__selected');
-});
+    items2[0].classList.add('carousel__item__selected');
+    buttons2[0].classList.add('carousel__button__selected');
+
 
