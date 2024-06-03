a934002697625a8d5bf2fdd55d427624ead26e9c
CarlosRobinDeCloet <carlosdecloet@gmail.com>
2024-06-03 16:59:54 +0200

    check fix

--- carousel.js
+++ carousel.js
@@ -1,6 +1,6 @@
 
 const carousel1 = document.getElementById("carousel1");
-    const items1 = carousel1.children;
+    const items1 = carousel1.childNodes;
     const buttonsHtml1 = Array.from(items1, () => {
         return `<span class='carousel__button'></span>`;
     });
